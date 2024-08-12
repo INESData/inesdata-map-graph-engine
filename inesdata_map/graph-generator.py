6fb288b054cfa0afec544a90c4a2ee41d0e1b561
vncc <vncc@gmv.com>
2024-08-12 11:43:49 +0200

    Call morph-kgc via subprocess instead of materialize function

--- inesdata_map/graph-generator.py
+++ inesdata_map/graph-generator.py
@@ -1,12 +1,13 @@
 import os.path
 import morph_kgc as mkgc
+import subprocess
 
-
+config_path = "/home/code/inesdata-map/kg-generation/inesdata_map/config.ini"
 data_source_type = "XML"
 db_url = None # "mysql+pymysql://user:password@localhost:3306/db_name"
-mappings_path = "/home/code/inesdata-map/test/data/input/mappings/gtfs-xml.rml.ttl"
+mappings_path = "/home/code/inesdata-map/kg-generation/inesdata_map/data/input/mappings/gtfs-xml.rml.ttl"
 
-output_path = "/home/code/inesdata-map/test/data/output/knowledge-graph-xml.nt"
+output_path = "/home/code/inesdata-map/kg-generation/inesdata_map/data/output/knowledge-graph-xml.nt"
 
 def generate_graph(data_source_type: str, mappings_path: str, output_path: str, db_url: str=None):
     # Define config.ini content
@@ -23,12 +24,16 @@ def generate_graph(data_source_type: str, mappings_path: str, output_path: str, 
         config += f"""
         db_url: {db_url}
         """
+    # save config.ini file
+    with open(config_path, "w") as config_file:
+        config_file.write(config)
 
     # Generate knowledge graph
     if not os.path.exists(output_path):
         f = open(output_path, 'x')
-    g = mkgc.materialize(config)
-    g.parse(output_path)
+    # g = mkgc.materialize(config)
+    # g.parse(output_path)
+    subprocess.run(["python3", "-m", "morph_kgc", config_path])
     
     print(f'Knowledge Graph generated: {output_path}')
 

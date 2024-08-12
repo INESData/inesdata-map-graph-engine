d8fcc68f0e11a38cc524a7dc6167e8498146f7dc
vncc <vncc@gmv.com>
2024-08-12 11:18:06 +0200

    Add comments

--- inesdata_map/graph-generator.py
+++ inesdata_map/graph-generator.py
@@ -9,6 +9,7 @@ mappings_path = "/home/code/inesdata-map/test/data/input/mappings/gtfs-xml.rml.t
 output_path = "/home/code/inesdata-map/test/data/output/knowledge-graph-xml.nt"
 
 def generate_graph(data_source_type: str, mappings_path: str, output_path: str, db_url: str=None):
+    # Define config.ini content
     config = f"""
     [CONFIGURATION]
     logging_level: DEBUG
@@ -17,15 +18,19 @@ def generate_graph(data_source_type: str, mappings_path: str, output_path: str, 
     [DataSource{data_source_type}]
     mappings: {mappings_path}
     """
+    # if input data source is DB, add its connection url
     if db_url:
         config += f"""
         db_url: {db_url}
         """
 
+    # Generate knowledge graph
     if not os.path.exists(output_path):
         f = open(output_path, 'x')
     g = mkgc.materialize(config)
     g.parse(output_path)
+    
     print(f'Knowledge Graph generated: {output_path}')
+
     
 generate_graph(data_source_type, mappings_path, output_path, db_url)
